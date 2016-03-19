0d95ecf5aed869f13781809148b3233a6ff0a10d
danaMaman <dana.maman1@gmail.com>
2016-03-19 17:40:29 +0200

    danas changes

--- MyCards/App_Start/BundleConfig.cs
+++ MyCards/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace MyCards
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/gmaps").Include(
+                     "~/Scripts/gmaps.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
