8462a1c003b9386f431bdb413c1946b8c412141e
danaMaman <dana.maman1@gmail.com>
2016-03-19 19:25:14 +0200

    add map:)

--- MyCards/Controllers/HomeController.cs
+++ MyCards/Controllers/HomeController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyCards.Models;
+using System.Data.Entity;
 
 namespace MyCards.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+
+            var addresses = db.Restuarants.Include(a => a.Location).ToArray();
+            string addressesString = "";
+
+            for (int i = 0; i < addresses.Count(); i++)
+            {
+                addressesString += addresses.ElementAt(i).Location.Address + " " + addresses.ElementAt(i).Location.City + "~";
+            }
+
+            //ViewBag.restaurantsData = addressesString.ToString();
+            ViewBag.restaurantsData = "קדושי מצרים יהוד";
+
             return View();
         }
 
