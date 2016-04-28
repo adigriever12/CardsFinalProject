e44d032b278ca9b457841097701dcd8807e98632
danaMaman <dana.maman1@gmail.com>
2016-04-28 15:27:41 +0300

    Add partial view for the list

--- MyCards/Scripts/site/site.js
+++ MyCards/Scripts/site/site.js
@@ -224,4 +224,11 @@ $(document).ready(function () {
     filtersChanged();
     recommendedClick();
     selectRanking();
+
+
+
+    $('#test').click(function () {
+        $("#testdiv").load("Home/_List");
+    });
+
 });
